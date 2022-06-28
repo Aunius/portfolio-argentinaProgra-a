c337ecfe846a8f0e3ecf7c58a66663f58b0a740c
Aunius <coronel.nahuel@outlook.com>
2022-06-28 13:06:45 -0300

    agregado del menu, paleta de colores y estructura general del proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { PoryectosComponent } from './components/poryectos/poryectos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabeceraComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    PoryectosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
