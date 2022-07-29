6af514cc931e33ec002e169c60f179456118208b
Aunius <coronel.nahuel@outlook.com>
2022-07-29 12:31:43 -0300

    Agregado de la carpeta backend con spring boot

---
+++ Backend/portfolioargprog/src/main/java/com/portfolio/portfolioargprog/PortfolioargprogApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.portfolioargprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioargprogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioargprogApplication.class, args);
+	}
+
+}
