fa40540512fdd77db275c4221e619f4a489d7bc0
Aunius <coronel.nahuel@outlook.com>
2022-08-02 18:37:03 -0300

    agregado del login y creacion de usuario

---
+++ Backend/portfolioargprog/src/main/java/com/portfolio/portfolioargprog/Security/Service/UserDetailsimp.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.portfolioargprog.Security.Service;
+
+import com.portfolio.portfolioargprog.Security.Entity.Usuario;
+import com.portfolio.portfolioargprog.Security.Entity.UsuarioPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsimp implements UserDetailsService {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.getByNombreUsuario(nombreUsuario).get();
+        return UsuarioPrincipal.build(usuario);
+    }
+}
