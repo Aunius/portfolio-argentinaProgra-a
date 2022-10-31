aefa58c95bfa378f2a82440348f68933840dab5f
Aunius <coronel.nahuel@outlook.com>
2022-10-30 22:29:02 -0300

    Proyectos

--- src/app/components/proyectos/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos/proyectos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Proyecto } from 'src/app/model/proyecto';
+import { InicioService } from 'src/app/service/inicio.service';
 
 @Component({
   selector: 'app-proyectos',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectosComponent implements OnInit {
 
-  constructor() { }
+  valores: Proyecto[] = [new Proyecto(-1,"No se ha podido cargar la información", "No se ha podido cargar la información","","")];
+
+  constructor(public inicioService: InicioService) { }
 
   ngOnInit(): void {
+    this.inicioService.proyectos().subscribe(data => {
+      this.valores = [];
+      data.proyectos.forEach((element: any) => {
+          this.valores.push(new Proyecto(element.id, element.titulo, element.subtitulo, element.url, element.url_imagen));
+      });
+    })
   }
-
 }
--- src/app/model/proyecto.ts
+++ src/app/model/proyecto.ts
@@ -1,11 +1,12 @@
 export class Proyecto {
-    id?: number;
+    id: number;
     subtitulo: string;
     titulo: string;
     url: string;
     url_imagen: string;
     
-    constructor(subtitulo:string, titulo:string, url:string, url_imagen:string){
+    constructor(id: number, titulo:string, subtitulo:string, url:string, url_imagen:string){
+        this.id = id;
         this.subtitulo = subtitulo;
         this.titulo = titulo;
         this.url = url;
